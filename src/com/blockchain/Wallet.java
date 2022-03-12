4c93d43b7f6aab5397a93798c8b1766dddce1e6a
Tuan Anh Le <anh.le@myob.com>
2022-03-12 15:14:19 +1100

    feat: Wallet balance

--- src/com/blockchain/Wallet.java
+++ src/com/blockchain/Wallet.java
@@ -2,10 +2,16 @@ package com.blockchain;
 
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>(); //only UTXOs owned by this wallet.
 
     public Wallet() {
         generateKeyPair();
@@ -25,6 +31,49 @@ public class Wallet {
         }catch(Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // Returns balance and stores the UTXO's owned by this wallet in this.UTXOs
+    public float getBalance() {
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: JavaChain.UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { // If output belongs to me ( if coins belong to me )
+                UTXOs.put(UTXO.id, UTXO); // Add it to our list of unspent transactions.
+                total += UTXO.value ;
+            }
+        }
+        return total;
+    }
+
+    // Generates and returns a new transaction from this wallet.
+    public Transaction sendFunds(PublicKey _recipient,float value ) {
+        if(getBalance() < value) { // Gather balance and check funds.
+            System.out.println("#Not Enough funds to send transaction. Transaction Discarded.");
+            return null;
+        }
+
+        // Create array list of inputs
+        ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+        // Collect UTXO until get enough amount
+        float total = 0;
+        for (Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            total += UTXO.value;
+            inputs.add(new TransactionInput(UTXO.id));
+            if (total > value) break;
+        }
+
+        Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+        newTransaction.generateSignature(privateKey);
+
+        // Remove spending ones from my input
+        for(TransactionInput input: inputs){
+            UTXOs.remove(input.transactionOutputId);
+        }
+
+        return newTransaction;
     }
 
 }
